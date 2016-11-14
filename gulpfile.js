5bedeb6a4081936b1a20bc62a9bcb8ea1fc10a70
Ilya Govorukhin <Govoruxin3@mail.ru>
2016-11-14 16:31:53 +0300

    newSetting

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,237 @@
-// $ npm install gulp gulp-jade gulp-less  gulp-imagemin  gulp-concat connect --save-dev
 
-var gulp = require('gulp'), // Сообственно Gulp JS
-    jade = require('gulp-jade'), // Плагин для Jade
-    less = require('gulp-less'), // Плагин для Stylus
-    imagemin = require('gulp-imagemin'), // Минификация изображений
-    concat = require('gulp-concat'); // Склейка файлов
+//
+//var gulp = require('gulp'), // Сообственно Gulp JS
+//    jade = require('gulp-jade'), // Плагин для Jade
+//    less = require('gulp-less'), // Плагин для Stylus
+//    imagemin = require('gulp-imagemin'), // Минификация изображений
+//    concat = require('gulp-concat'); // Склейка файлов
+//
+//
+////  less
+//gulp.task('less', function() {
+//    gulp.src('app/css/*.less')
+//
+//});
+//
+//// С Jade
+//
+//    gulp.task('jade', function() {
+//        gulp.src('app/*.jade')
+//
+//    });
+//
+//
+//
+//// JS
+//    gulp.task('js', function() {
+//        gulp.src('app/js/*.*')
+//
+//
+//    });
+//
+//
+//
+////  изображения
+//
+//    gulp.task('images', function() {
+//        gulp.src('app/img/*')
+//
+//
+//    });
+//// font
+//
+//gulp.task('font', function() {
+//    gulp.src('app/font/*')
+//
+//
+//});
+//
+//gulp.task('audio', function() {
+//    gulp.src('app/audio/*')
+//
+//
+//});
+//
+//// Запуск сервера разработки gulp watch
+//    gulp.task('watch', function() {
+//        // Предварительная сборка проекта
+//        gulp.watch('less');
+//        gulp.watch('jade');
+//        gulp.watch('images');
+//        gulp.watch('js');
+//
+//
+//    });
+//
+//
+//
+//    gulp.task('build', function() {
+//        // css
+//        gulp.src('app/css/*.less')
+//            .pipe(less())
+//            .pipe(gulp.dest('dist/css/'));
+//
+//        // jade
+//        gulp.src('app/*.jade')
+//            .pipe(jade({
+//            pretty: true
+//        }))  // Собираем Jade только в папке app
+//            .pipe(gulp.dest('dist/'));
+//
+//        // js
+//        gulp.src('app/js/*.*')
+//            .pipe(gulp.dest('dist/js'));
+//
+//
+//
+//        // image
+//        gulp.src('app/img/*')
+//            .pipe(imagemin())
+//            .pipe(gulp.dest('dist/img'));
+//
+//        // шрифт
+//        gulp.src('app/font/*')
+//            .pipe(gulp.dest('dist/font'));
+//
+//
+//    });
+//
+//gulp.task('default', ['watch']);
 
 
-//  less
-gulp.task('less', function() {
-    gulp.src('app/css/*.less')
+'use strict';
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    less = require('gulp-less'),
+    jade = require('gulp-jade'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
 
+var path = {
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        html: 'app/*.jade',
+        js: 'app/js/*.js',
+        style: 'app/css/*.less',
+        img: 'app/img/**/*.*',
+        fonts: 'app/fonts/**/*.*'
+    },
+    watch: {
+        html: 'app/*.jade',
+        js: 'app/js/*.js',
+        style: 'app/css/common/*.less',
+        img: 'app/img/**/*.*',
+        fonts: 'app/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+gulp.task('webserver', function () {
+    browserSync(config);
 });
 
-// С Jade
-
-    gulp.task('jade', function() {
-        gulp.src('app/*.jade')
-
-    });
-
-
-
-// JS
-    gulp.task('js', function() {
-        gulp.src('app/js/*.*')
-
-
-    });
-
-
-
-//  изображения
-
-    gulp.task('images', function() {
-        gulp.src('app/img/*')
-
-
-    });
-// font
-
-gulp.task('font', function() {
-    gulp.src('app/font/*')
-
-
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
 });
 
-gulp.task('audio', function() {
-    gulp.src('app/audio/*')
-
-
-});
-
-// Запуск сервера разработки gulp watch
-    gulp.task('watch', function() {
-        // Предварительная сборка проекта
-        gulp.watch('less');
-        gulp.watch('jade');
-        gulp.watch('images');
-        gulp.watch('js');
-
-
-    });
-
-
-
-    gulp.task('build', function() {
-        // css
-        gulp.src('app/css/*.less')
-            .pipe(less())
-            .pipe(gulp.dest('dist/css/'));
-
-        // jade
-        gulp.src('app/*.jade')
-            .pipe(jade({
+gulp.task('html:build', function () {
+    gulp.src(path.src.html)
+        .pipe(jade({
             pretty: true
-        }))  // Собираем Jade только в папке app
-            .pipe(gulp.dest('dist/'));
+        }))
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
+});
 
-        // js
-        gulp.src('app/js/*.*')
-            .pipe(gulp.dest('dist/js'));
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style)
+        .pipe(sourcemaps.init())
+        .pipe(less({
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'style:build',
+    'fonts:build',
+    'image:build'
+]);
 
 
-
-        // image
-        gulp.src('app/img/*')
-            .pipe(imagemin())
-            .pipe(gulp.dest('dist/img'));
-
-        // шрифт
-        gulp.src('app/font/*')
-            .pipe(gulp.dest('dist/font'));
-
-
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
     });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
 
-gulp.task('default', ['watch']);
+
+gulp.task('default', ['build', 'webserver', 'watch']);
