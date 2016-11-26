72d7bef7ca5487dc7d731eb90fc75f05283a5a86
Ilya Govorukhin <Govoruxin3@mail.ru>
2016-11-26 05:03:13 +0300

    add some miss

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,4 @@
 
-//
-//var gulp = require('gulp'), // Сообственно Gulp JS
-//    jade = require('gulp-jade'), // Плагин для Jade
-//    less = require('gulp-less'), // Плагин для Stylus
-//    imagemin = require('gulp-imagemin'), // Минификация изображений
-//    concat = require('gulp-concat'); // Склейка файлов
-//
-//
-////  less
-//gulp.task('less', function() {
-//    gulp.src('app/css/*.less')
-//
-//});
-//
-//// С Jade
-//
-//    gulp.task('jade', function() {
-//        gulp.src('app/*.jade')
-//
-//    });
-//
-//
-//
-//// JS
-//    gulp.task('js', function() {
-//        gulp.src('app/js/*.*')
-//
-//
-//    });
-//
-//
-//
-////  изображения
-//
-//    gulp.task('images', function() {
-//        gulp.src('app/img/*')
-//
-//
-//    });
-//// font
-//
-//gulp.task('font', function() {
-//    gulp.src('app/font/*')
-//
-//
-//});
-//
-//gulp.task('audio', function() {
-//    gulp.src('app/audio/*')
-//
-//
-//});
-//
-//// Запуск сервера разработки gulp watch
-//    gulp.task('watch', function() {
-//        // Предварительная сборка проекта
-//        gulp.watch('less');
-//        gulp.watch('jade');
-//        gulp.watch('images');
-//        gulp.watch('js');
-//
-//
-//    });
-//
-//
-//
-//    gulp.task('build', function() {
-//        // css
-//        gulp.src('app/css/*.less')
-//            .pipe(less())
-//            .pipe(gulp.dest('dist/css/'));
-//
-//        // jade
-//        gulp.src('app/*.jade')
-//            .pipe(jade({
-//            pretty: true
-//        }))  // Собираем Jade только в папке app
-//            .pipe(gulp.dest('dist/'));
-//
-//        // js
-//        gulp.src('app/js/*.*')
-//            .pipe(gulp.dest('dist/js'));
-//
-//
-//
-//        // image
-//        gulp.src('app/img/*')
-//            .pipe(imagemin())
-//            .pipe(gulp.dest('dist/img'));
-//
-//        // шрифт
-//        gulp.src('app/font/*')
-//            .pipe(gulp.dest('dist/font'));
-//
-//
-//    });
-//
-//gulp.task('default', ['watch']);
 
 
 'use strict';
@@ -191,12 +93,7 @@ gulp.task('style:build', function () {
 
 gulp.task('image:build', function () {
     gulp.src(path.src.img)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()],
-            interlaced: true
-        }))
+        .pipe(imagemin())
         .pipe(gulp.dest(path.build.img))
         .pipe(reload({stream: true}));
 });
